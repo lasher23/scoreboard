8b1cf0f7f073b8f21b707a5461eef375545d6365
Nicolas Schmid <nicolas.schmid@bison-group.com>
2018-05-04 08:59:04 +0200

    Scoreboard not to Primary monitor

--- ScoreBoard/src/main/java/ch/uhc_yetis/Scoreboard.java
+++ ScoreBoard/src/main/java/ch/uhc_yetis/Scoreboard.java
@@ -1,3 +1,4 @@
+
 package ch.uhc_yetis;
 
 import ch.uhc_yetis.view.ScoreboardController;
@@ -12,39 +13,39 @@ import javafx.stage.Stage;
 
 public class Scoreboard extends Application {
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+  public static void main(String[] args) {
+    launch(args);
+  }
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		Font.loadFont(getClass().getResourceAsStream("view/DSEG14-Modern/DSEG14Modern-Regular.ttf"), 250);
-		Font.loadFont(getClass().getResourceAsStream("view/DSEG14-Modern/DSEG14Modern-Regular.ttf"), 150);
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Font.loadFont(getClass().getResourceAsStream("view/DSEG14-Modern/DSEG14Modern-Regular.ttf"), 250);
+    Font.loadFont(getClass().getResourceAsStream("view/DSEG14-Modern/DSEG14Modern-Regular.ttf"), 150);
 
-		ScoreboardController sc = new ScoreboardController();
-		ScoreboardControllerController scc = new ScoreboardControllerController(sc);
-		sc.setOnCloseRequest(event -> scc.close());
-		scc.setOnCloseRequest(event -> sc.close());
-		StyleController styleController = new StyleController(sc.getRoot());
-		scc.getScene().setOnKeyPressed(event -> {
-			if (!styleController.isShowing() && event.getCode().equals(KeyCode.F11)) {
-				styleController.show();
-			}
-		});
-		sc.show();
-		scc.show();
-		Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
-		scc.setX(visualBounds.getMinX());
-		scc.setY(visualBounds.getMinY());
+    ScoreboardController sc = new ScoreboardController();
+    ScoreboardControllerController scc = new ScoreboardControllerController(sc);
+    sc.setOnCloseRequest(event -> scc.close());
+    scc.setOnCloseRequest(event -> sc.close());
+    StyleController styleController = new StyleController(sc.getRoot());
+    scc.getScene().setOnKeyPressed(event -> {
+      if (!styleController.isShowing() && event.getCode().equals(KeyCode.F11)) {
+        styleController.show();
+      }
+    });
+    sc.show();
+    scc.show();
+    Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
+    scc.setX(visualBounds.getMinX());
+    scc.setY(visualBounds.getMinY());
 
-		for (Screen screen : Screen.getScreens()) {
-			if (screen.equals(Screen.getPrimary())) {
-				sc.setX(screen.getVisualBounds().getMinX());
-				sc.setY(screen.getVisualBounds().getMinY());
-			}
-		}
+    for (Screen screen : Screen.getScreens()) {
+      if (!screen.equals(Screen.getPrimary())) {
+        sc.setX(screen.getVisualBounds().getMinX());
+        sc.setY(screen.getVisualBounds().getMinY());
+      }
+    }
 
-		sc.setMaximized(true);
-		scc.setMaximized(true);
-	}
+    sc.setMaximized(true);
+    scc.setMaximized(true);
+  }
 }
